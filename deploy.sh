b51c0fdf3d099ae32cdcc6b04e7a644c46ad35ad
Max Cascone <max@bitovi.com>
2023-01-20 16:00:18 -0600

    wip
    DEVOPS-427

--- deploy.sh
+++ deploy.sh
@@ -10,23 +10,26 @@ export GCP_ROOT_OPERATIONS="$BITOPS_OPSREPO_ENVIRONMENT_DIR"
 export BITOPS_SCHEMA_ENV_FILE="$GCP_ROOT_OPERATIONS/ENV_FILE"
 export SCRIPTS_DIR="$GCP_ROOT_SCRIPTS/scripts"
 
-echo "BITOPS_INSTALLED_PLUGIN_DIR: $BITOPS_INSTALLED_PLUGIN_DIR"
-echo "BITOPS_PLUGIN_DIR: $BITOPS_PLUGIN_DIR"
-echo "GCP_ROOT_SCRIPTS: $GCP_ROOT_SCRIPTS"
-echo "GCP_ROOT_OPERATIONS: $GCP_ROOT_OPERATIONS"
-echo "BITOPS_SCHEMA_ENV_FILE: $BITOPS_SCHEMA_ENV_FILE"
-echo "SCRIPTS_DIR: $SCRIPTS_DIR"
-echo 
+if [ ! -z "$BITOPS_DEBUG" ]; then
+  echo "BITOPS_INSTALLED_PLUGIN_DIR: $BITOPS_INSTALLED_PLUGIN_DIR"
+  echo "BITOPS_PLUGIN_DIR: $BITOPS_PLUGIN_DIR"
+  echo "GCP_ROOT_SCRIPTS: $GCP_ROOT_SCRIPTS"
+  echo "GCP_ROOT_OPERATIONS: $GCP_ROOT_OPERATIONS"
+  echo "BITOPS_OPSREPO_ENVIRONMENT_DIR: $BITOPS_OPSREPO_ENVIRONMENT_DIR"
+  echo "BITOPS_SCHEMA_ENV_FILE: $BITOPS_SCHEMA_ENV_FILE"
+  echo "SCRIPTS_DIR: $SCRIPTS_DIR"
+  echo
+fi
 
 if [ ! -d "$GCP_ROOT_OPERATIONS" ]; then
   echo "No gcp directory. Skipping."
   exit 0
 fi
 
-printf "Deploying gcp..."
+echo "Deploying gcp..."
 
 if [ ! -f "$BITOPS_SCHEMA_ENV_FILE" ]; then 
-  echo "No gcp ENV file found"
+  echo "No gcp ENV file found... skipping source"
 else
   source "$BITOPS_SCHEMA_ENV_FILE"
 fi
@@ -38,6 +41,6 @@ cd $GCP_ROOT_OPERATIONS
 echo "Listing contents of gcp Root: $GCP_ROOT_OPERATIONS"
 ls -al .
 
-echo "settings:"
-echo "GCP_FOO"
-echo "$GCP_FOO"
+echo "Ops Repo Env Vars:"
+echo "GCP_FOO: $GCP_FOO"
+echo
