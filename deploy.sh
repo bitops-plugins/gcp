99c9720575f0ea4f6c3b5f1db3d4f7ace23e930e
mcascone <max@bitovi.com>
2023-05-16 13:42:43 -0500

    no-install works

--- deploy.sh
+++ deploy.sh
@@ -1,49 +1,36 @@
 #!/bin/bash
 set -e
 
-echo "Running GCP Plugin deployment script..."
-echo
+PLUGIN_NAME="GCP"
+PLUGIN_CLI="gcloud"
+
+echo "Running $PLUGIN_NAME Plugin deployment script..."
 
 # vars
-export GCP_ROOT_SCRIPTS="$BITOPS_INSTALLED_PLUGIN_DIR"
-export GCP_ROOT_OPERATIONS="$BITOPS_OPSREPO_ENVIRONMENT_DIR"
-export BITOPS_SCHEMA_ENV_FILE="$GCP_ROOT_OPERATIONS/ENV_FILE"
-export SCRIPTS_DIR="$GCP_ROOT_SCRIPTS/scripts"
+export BITOPS_SCHEMA_ENV_FILE="$BITOPS_OPSREPO_ENVIRONMENT_DIR/ENV_FILE"
 
-if [ ! -z "$BITOPS_DEBUG" ]; then
-  echo "BITOPS_INSTALLED_PLUGIN_DIR: $BITOPS_INSTALLED_PLUGIN_DIR"
-  echo "BITOPS_PLUGIN_DIR: $BITOPS_PLUGIN_DIR"
-  echo "GCP_ROOT_SCRIPTS: $GCP_ROOT_SCRIPTS"
-  echo "GCP_ROOT_OPERATIONS: $GCP_ROOT_OPERATIONS"
-  echo "BITOPS_OPSREPO_ENVIRONMENT_DIR: $BITOPS_OPSREPO_ENVIRONMENT_DIR"
-  echo "BITOPS_SCHEMA_ENV_FILE: $BITOPS_SCHEMA_ENV_FILE"
-  echo "SCRIPTS_DIR: $SCRIPTS_DIR"
-  echo
-fi
-
-if [ ! -d "$GCP_ROOT_OPERATIONS" ]; then
-  echo "No gcp directory. Skipping."
+if [ ! -d "$BITOPS_OPSREPO_ENVIRONMENT_DIR" ]; then
+  echo "No $PLUGIN_NAME plugin directory. Skipping."
   exit 0
 fi
 
-echo "Deploying gcp..."
+echo "Deploying $PLUGIN_NAME plugin..."
 
 if [ ! -f "$BITOPS_SCHEMA_ENV_FILE" ]; then 
-  echo "No gcp ENV file found... skipping source"
+  echo "No $PLUGIN_NAME plugin ENV file found"
 else
   source "$BITOPS_SCHEMA_ENV_FILE"
 fi
 
-bash $SCRIPTS_DIR/validate_env.sh
+cd $BITOPS_OPSREPO_ENVIRONMENT_DIR
 
-cd $GCP_ROOT_OPERATIONS
+echo "Listing contents of $PLUGIN_NAME plugin Root: $BITOPS_OPSREPO_ENVIRONMENT_DIR"
+ls -l
 
-echo
-echo "Listing contents of gcp Root: $GCP_ROOT_OPERATIONS"
-ls -al .
-echo
+echo "Options:"
+echo "GCP_FOO: $GCP_FOO"
+echo "GCP_BAR: $GCP_BAR"
 
-echo
-echo "Ops Repo Env Vars:"
-echo "BITOPS_GCP_FOO: $BITOPS_GCP_FOO"
-echo
+# here is the image using the installed cli
+echo "Running the plugin CLI... later"
+# $PLUGIN_CLI version
